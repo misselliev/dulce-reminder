e062f27719059567e9375df9f9f0a7400476549d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-22 19:16:03 -0700

    generating layout items, cleaning button func

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,67 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+window.onload = function() {
+  const container = document.getElementById('main');
+  const btnCont = Object.assign(
+    document.createElement('div'),
+    { className: 'container' },
+    { id: 'btn-container' }
+  );
+  const btn_create = Object.assign(
+    document.createElement('button'),
+    { className: 'btn' },
+    { id: 'btn-create' },
+    { innerText: 'Create Task' }
+  );
+
+  const btn_clear = Object.assign(
+    document.createElement('button'),
+    { className: 'btn' },
+    { id: 'btn-clear' },
+    { innerText: 'Clear List' }
+  );
+
+  const listCont = Object.assign(
+    document.createElement('div'),
+    { className: 'container' },
+    { id: 'list-container' }
+  );
+  const listUl = Object.assign(document.createElement('ul'), { id: 'list-ul' });
+
+  container.appendChild(btnCont);
+  btnCont.appendChild(btn_create);
+  btnCont.appendChild(btn_clear);
+  container.appendChild(listCont);
+  listCont.appendChild(listUl);
+
+  btn_create.addEventListener('click', storeData);
+  btn_clear.addEventListener('click', clearData);
+};
+let counter = 1;
+let todolist = [];
+
+function storeData() {
+  let input = document.getElementById('todo').value;
+  localStorage.setItem(counter, input);
+  console.log('storing your data...');
+  todolist.push(input);
+  displayData(todolist);
+  counter += 1;
+}
+
+function displayData(array) {
+  console.log('displaying your to-dos...');
+  const list = document.getElementById('list-ul');
+  array.forEach((element) => {
+    item_list = document.createElement('li');
+    item_list.innerText = localStorage.getItem(element);
+    list.appendChild(item_list);
+  });
+}
+
+function clearData() {
+  console.log('cleaning time!');
+  localStorage.clear();
+}
